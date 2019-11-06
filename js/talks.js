0667250708a83c76c6374610e06e78189fdf15c8
Hendrik <mail@hendrikwallbaum.de>
2019-11-06 12:50:04 +0100

    :books: add recodings for testing talk.

--- js/talks.js
+++ js/talks.js
@@ -1,55 +1,89 @@
-export default [{
-  title: '🧐 Testing webapps',
-  subtitle: 'The whys and hows of testing and Cypress',
-  description: 'A talk about why you should do end to end (e2e) testing for your webapps and how to use Cypress for e2e-testing. We will gather motivation to get started with testing before diving into Cypress as an amazing tool to get the job done.',
-  links: [{
-    href: 'http://testing-webapps.talks.hoverbaum.net/',
-    text: 'HTML based presentation'
-  }, {
-    href: '/pdf/testing-webapps-nl-and-connects.pdf',
-    text: 'As PDF (NL and Hamburg connects)'
-  }, {
-    href: '/pdf/testing-webapps-charlie.pdf',
-    text: 'As PDF (charlie)'
-  }, {
-    href: 'https://github.com/HoverBaum/talk-testing-during-development',
-    text: 'GitHub Repo'
-  }],
-  year: 2019
-}, {
-  title: '😍 How I fell in love with an APi first CMS',
-  subtitle: 'The rise of fun CMS development',
-  description: 'We take a look at the historic development of CMS solutions and arive at API first CMS systems.',
-  links: [{
-    href: 'https://docs.google.com/presentation/d/1NqdL0ILZqqbQSqw4yM0462jHxl3ER3JjdmfhicuQLBs/edit?usp=drivesdk',
-    text: 'Online slideshow'
-  }, {
-    href: '/pdf/api-first-cms.pdf',
-    text: 'As PDF'
-  }],
-  year: 2018
-}, {
-  title: '🔩 Node based CLIs',
-  subtitle: 'Being awesome in JavaScript',
-  description: 'This talk takes a look at a range of great, NodeJS based command line interfaces. After exploring all that awesomeness, we also take a look at a way to build out own.',
-  links: [{
-    href: 'https://docs.google.com/presentation/d/1EpF_EJXtb6vIjXG5Vone7kc6cZhrZLDhmUnTYkYP_4M/edit?usp=drivesdk',
-    text: 'Online slideshow'
-  }, {
-    href: '/pdf/node-based-cli.pdf',
-    text: 'As PDF'
-  }],
-  year: 2017
-}, {
-  title: '🚂 ChooJS',
-  subtitle: 'Introducing choo',
-  description: 'An introduction to ChooJS. A framework combining the best practices learned in React to create a fun developer experience.',
-  links: [{
-    href: 'https://docs.google.com/presentation/d/1fTcz2TzSUa5nnXy-AfAaWS-_GwSxZ3QY9OG5KRmJQAI/edit?usp=sharing',
-    text: 'Online slideshow'
-  }, {
-    href: '/pdf/choo-frontend.pdf',
-    text: 'As PDF'
-  }],
-  year: 2017
-}]
+export default [
+  {
+    title: '🧐 Testing webapps',
+    subtitle: 'The whys and hows of testing and Cypress',
+    description:
+      'A talk about why you should do end to end (e2e) testing for your webapps and how to use Cypress for e2e-testing. We will gather motivation to get started with testing before diving into Cypress as an amazing tool to get the job done.',
+    links: [
+      {
+        href: 'http://testing-webapps.talks.hoverbaum.net/',
+        text: 'HTML based presentation'
+      },
+      {
+        href: 'https://www.youtube.com/watch?v=ZlHxwSJoCZM',
+        text: 'Recording: code.talks 2019'
+      },
+      {
+        href: 'https://www.youtube.com/watch?v=u2_rljUQArs',
+        text: 'Recording: Softwerkskammer 2019'
+      },
+      {
+        href: '/pdf/testing-webapps-nl-and-connects.pdf',
+        text: 'As PDF (NL and Hamburg connects)'
+      },
+      {
+        href: '/pdf/testing-webapps-charlie.pdf',
+        text: 'As PDF (charlie)'
+      },
+      {
+        href: 'https://github.com/HoverBaum/talk-testing-during-development',
+        text: 'GitHub Repo'
+      }
+    ],
+    year: 2019
+  },
+  {
+    title: '😍 How I fell in love with an APi first CMS',
+    subtitle: 'The rise of fun CMS development',
+    description:
+      'We take a look at the historic development of CMS solutions and arive at API first CMS systems.',
+    links: [
+      {
+        href:
+          'https://docs.google.com/presentation/d/1NqdL0ILZqqbQSqw4yM0462jHxl3ER3JjdmfhicuQLBs/edit?usp=drivesdk',
+        text: 'Online slideshow'
+      },
+      {
+        href: '/pdf/api-first-cms.pdf',
+        text: 'As PDF'
+      }
+    ],
+    year: 2018
+  },
+  {
+    title: '🔩 Node based CLIs',
+    subtitle: 'Being awesome in JavaScript',
+    description:
+      'This talk takes a look at a range of great, NodeJS based command line interfaces. After exploring all that awesomeness, we also take a look at a way to build out own.',
+    links: [
+      {
+        href:
+          'https://docs.google.com/presentation/d/1EpF_EJXtb6vIjXG5Vone7kc6cZhrZLDhmUnTYkYP_4M/edit?usp=drivesdk',
+        text: 'Online slideshow'
+      },
+      {
+        href: '/pdf/node-based-cli.pdf',
+        text: 'As PDF'
+      }
+    ],
+    year: 2017
+  },
+  {
+    title: '🚂 ChooJS',
+    subtitle: 'Introducing choo',
+    description:
+      'An introduction to ChooJS. A framework combining the best practices learned in React to create a fun developer experience.',
+    links: [
+      {
+        href:
+          'https://docs.google.com/presentation/d/1fTcz2TzSUa5nnXy-AfAaWS-_GwSxZ3QY9OG5KRmJQAI/edit?usp=sharing',
+        text: 'Online slideshow'
+      },
+      {
+        href: '/pdf/choo-frontend.pdf',
+        text: 'As PDF'
+      }
+    ],
+    year: 2017
+  }
+]
